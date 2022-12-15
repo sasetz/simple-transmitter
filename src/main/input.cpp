fbf04bccdc8703f4a26c808f35fd69cc2395c197
sasetz <56696451a@gmail.com>
2022-12-15 02:35:09 +0100

    Changed tiny input bug

--- src/main/input.cpp
+++ src/main/input.cpp
@@ -11,7 +11,7 @@ void listen(uint16_t port, uint16_t fragmentLength) {
     transmitter.run(false);
 
     int option;
-    while(!transmitter.isClosing()) {
+    while (!transmitter.isClosing()) {
         std::cout << "Please, choose your option:\n";
         std::cout << "[0] Close connection\n";
         std::cout << "[1] Send a file\n";
@@ -19,16 +19,16 @@ void listen(uint16_t port, uint16_t fragmentLength) {
         std::cout << "[3] Check if something arrived\n";
 
         std::cin >> option;
-        if(option == 0) {
+        if (option == 0) {
             std::cout << "Closing the connection...\n";
             transmitter.close();
-        } else if(option == 1) {
+        } else if (option == 1) {
             std::cout << "Input file path: ";
             std::string path;
             std::cin >> path;
             transmitter.addFile(path);
             std::cout << "Sending your file...\n";
-        } else if(option == 2) {
+        } else if (option == 2) {
             std::cout << "Input text: ";
             std::string text;
             std::cin >> text;
@@ -36,7 +36,7 @@ void listen(uint16_t port, uint16_t fragmentLength) {
             std::cout << "Sending your text...\n";
         } else {
             auto output = transmitter.getOutput();
-            if(!output) {
+            if (!output) {
                 std::cout << "No data received\n";
             } else {
                 std::cout << output.value();
@@ -45,13 +45,13 @@ void listen(uint16_t port, uint16_t fragmentLength) {
     }
 }
 
-void connect(const std::string& externalSocket, uint16_t fragmentLength) {
+void connect(const std::string &externalSocket, uint16_t fragmentLength) {
     Transmitter transmitter(SocketAddress(externalSocket, 0), fragmentLength);
     // since we don't know our host, yet
     transmitter.run(true);
 
     int option;
-    while(!transmitter.isClosing()) {
+    while (!transmitter.isClosing()) {
         std::cout << "Please, choose your option:\n";
         std::cout << "[0] Close connection\n";
         std::cout << "[1] Send a file\n";
@@ -59,16 +59,16 @@ void connect(const std::string& externalSocket, uint16_t fragmentLength) {
         std::cout << "[3] Check if something arrived\n";
 
         std::cin >> option;
-        if(option == 0) {
+        if (option == 0) {
             std::cout << "Closing the connection...\n";
             transmitter.close();
-        } else if(option == 1) {
+        } else if (option == 1) {
             std::cout << "Input file path: ";
             std::string path;
             std::cin >> path;
             transmitter.addFile(path);
             std::cout << "Sending your file...\n";
-        } else if(option == 2) {
+        } else if (option == 2) {
             std::cout << "Input text: ";
             std::string text;
             std::cin >> text;
@@ -76,7 +76,7 @@ void connect(const std::string& externalSocket, uint16_t fragmentLength) {
             std::cout << "Sending your text...\n";
         } else {
             auto output = transmitter.getOutput();
-            if(!output) {
+            if (!output) {
                 std::cout << "No data received\n";
             } else {
                 std::cout << output.value();
@@ -91,34 +91,37 @@ void interactivePrompt() {
     uint16_t fragmentLength = 512;
     int option = -1;
 
-    std::cout << "Please, choose your option:\n";
-    std::cout << "[0] Exit\n";
-    std::cout << "[1] Start listening for connections\n";
-    std::cout << "[2] Start connection\n";
-    std::cout << "[3] Change fragment length (512 by default)\n";
 
-    std::cin >> option;
+    do {
+        std::cout << "Please, choose your option:\n";
+        std::cout << "[0] Exit\n";
+        std::cout << "[1] Start listening for connections\n";
+        std::cout << "[2] Start connection\n";
+        std::cout << "[3] Change fragment length (512 by default)\n";
 
-    switch (option) {
-        case 0:
-            std::cout << "Exiting...\n";
-            break;
-        case 1:
-            std::cout << "Please, specify the port number on which your server should operate: ";
-            std::cin >> port;
-            listen(port, fragmentLength);
-            break;
-        case 2:
-            std::cout << "Input socket value (127.0.0.1:1111): ";
-            std::cin >> externalSocket;
-            std::cout << "Connecting to the client...\n";
-            connect(externalSocket, fragmentLength);
-            break;
-        case 3:
-            std::cout << "Please, input new fragment length: ";
-            std::cin >> fragmentLength;
-            break;
-        default:
-            std::cout << "Invalid option!\n";
-    }
+        std::cin >> option;
+
+        switch (option) {
+            case 0:
+                std::cout << "Exiting...\n";
+                break;
+            case 1:
+                std::cout << "Please, specify the port number on which your server should operate: ";
+                std::cin >> port;
+                listen(port, fragmentLength);
+                break;
+            case 2:
+                std::cout << "Input socket value (127.0.0.1:1111): ";
+                std::cin >> externalSocket;
+                std::cout << "Connecting to the client...\n";
+                connect(externalSocket, fragmentLength);
+                break;
+            case 3:
+                std::cout << "Please, input new fragment length: ";
+                std::cin >> fragmentLength;
+                break;
+            default:
+                std::cout << "Invalid option!\n";
+        }
+    } while (option == 3); // prevent from going out of the menu if the user chose to change the fragment
 }
